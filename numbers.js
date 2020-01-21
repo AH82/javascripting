ff4b5ea1f11e0433d5338e3ba6c6613cae133732
Hatem <hatem.deux@gmail.com>
2020-01-21 07:05:47 +0000

    Initial commit with all my work so far

---
+++ numbers.js
@@ -0,0 +1,4 @@
+let numbers = 123456789 
+// Official answer had the var keyword
+// var numbers = 123456789
+console.log(numbers)
---
+++ rounding-numbers.js
@@ -0,0 +1,10 @@
+var roundUp = 1.5
+var rounded = Math.round(roundUp)
+console.log(rounded)
+
+
+// official solution
+// const roundUp = 1.5
+// const rounded = Math.round(roundUp)
+// console.log(rounded)
+
